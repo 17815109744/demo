f417b3c9191060872a7198d23adb15fe91878fb7
17815109744 <145094781+17815109744@users.noreply.github.com>
2025-03-02 19:28:16 +0800

    Delete demo directory

--- demo/src/main/java/com/example/studentmanagement/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(String roleName); // 自定义查询方法，按角色名称查找
-}
